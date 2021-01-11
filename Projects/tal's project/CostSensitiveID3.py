6fe8b1517ef3b5129f1614f7cd481066f2d7255e
exital <talrosen3@gmail.com>
2021-01-11 13:42:21 +0200

    tal's project - added docstrings to the function is CostSensitiveID3

--- Projects/tal's project/CostSensitiveID3.py
+++ Projects/tal's project/CostSensitiveID3.py
@@ -2,17 +2,30 @@ from ID3 import ID3Node, ID3Classifier
 import argparse
 from utils import csv2xy, log, DEFAULT_CLASSIFICATION, graphPlotAndShow
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 
 class ID3CostSensitiveClassifier(ID3Classifier):
+    """
+    This classifier builds a regular ID3Tree and than pruning the nodes if it will improve loss costs.
+    """
     def __init__(self, cost_fn=10, cost_fp=1):
         super().__init__()
         self.validation = None
         self.cost_FN = cost_fn
         self.cost_FP = cost_fp
 
-    def fit(self, x, y):
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.60, random_state=420)
+    def fit(self, x, y, test_size=0.6):
+        """
+        Builds an ID3Tree and than prune it to improve costs
+        :param x: dataset
+        :type x: dataframe
+        :param y: dataset
+        :type y: dataframe
+        :param test_size: the fraction for splitting
+        :type test_size: float
+        """
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=420)
         train_data = X_train.copy()
         train_data["diagnosis"] = y_train
         validation_data = X_test.copy()
@@ -24,19 +37,30 @@ class ID3CostSensitiveClassifier(ID3Classifier):
         self.id3tree = pruned_tree
 
     def prune(self, node: ID3Node, validation):
-        if len(validation.index) == 0:
-            return node
-        if node.is_leaf():
+        """
+        This is the function that is pruning the tree for better costs loss
+        :param node: The tree to prune
+        :type node: ID3Node
+        :param validation: validation data to test on the node
+        :type validation: dataframe
+        :return: A pruned tree with better loss results
+        :rtype: ID3Node
+        """
+        # if no validation data or node is leaf we return the same node
+        if len(validation.index) == 0 or node.is_leaf():
             return node
 
         # slicing the dataframe
         validation_left = validation[validation[node.feature] <= node.slicing_val]
         validation_right = validation[validation[node.feature] > node.slicing_val]
 
+        # recursively pruning the node's sons
         node.left = self.prune(node.left, validation_left)
         node.right = self.prune(node.right, validation_right)
+
+        # checking if its better to prune that node or not with cost in mind
         err_prune, err_no_prune = 0, 0
-        prune_diagnostic = self.decide_leaf_diagnosis_by_costs(node, validation)
+        prune_diagnostic = self.decide_leaf_diagnosis_by_costs(validation)
         for row in range(len(validation.index)):
             prediction = self.walk_the_tree(node, row, validation)
             real_truth = validation["diagnosis"].iloc[row]
@@ -54,23 +78,76 @@ class ID3CostSensitiveClassifier(ID3Classifier):
         return node
 
     def evaluate(self, real_truth, predicted_truth):
+        """
+        evaluates the loss for the prediction of that node.
+        :param real_truth: the diagnosis of the node
+        :type real_truth: str
+        :param predicted_truth: the ID3Tree prediction of that node
+        :type predicted_truth: str
+        :return: the cost of that prediction
+        :rtype: int
+        """
         if real_truth != predicted_truth:
             return self.cost_FN if real_truth == "M" else self.cost_FP
         else:
             return 0
 
-    def decide_leaf_diagnosis_by_costs(self, node: ID3Node, validation):
+    def decide_leaf_diagnosis_by_costs(self, validation):
+        """
+        This function decides whats best diagnosis to give to a pruned node by the cost of it.
+        :param validation: the validation data of the node being checked
+        :type validation: dataframe
+        :return: the node diagnosis
+        :rtype: str
+        """
         data = validation
         count = data["diagnosis"].value_counts()
-        if "M" in count.index:
-            m_count = count["M"]
-        else:
-            m_count = 0
-        if "B" in count.index:
-            b_count = count["B"]
-        else:
-            b_count = 0
+        m_count = count["M"] if "M" in count.index else 0
+        b_count = count["B"] if "B" in count.index else 0
+
         return "M" if b_count * self.cost_FP < m_count * self.cost_FN else "B"
+
+
+def experiment(X=None, y=None, test_size=None, verbose=False):
+    """
+    This function uses sklearn's kFold to cross validate and find the best
+    size of split for costs sensitive
+    The only parameter you need is to set verbose to True so you can see output.
+    :param X: X dataset
+    :type X: dataframe
+    :param y: y dataset
+    :type y: dataframe
+    :param test_size: values to cross validate
+    :type test_size: list
+    :param verbose: True if you want to see graph and summary
+    :type verbose: bool
+    """
+    if X is None or y is None:
+        X, y = csv2xy("train.csv")
+    if test_size is None:
+        test_size = [x/100 for x in range(1, 99)]
+    num_of_splits = 2
+    loss_per_split = []
+    kf = KFold(n_splits=num_of_splits, random_state=307965806, shuffle=True)
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        loss_per_size = []
+        for size in test_size:
+            classifier = ID3CostSensitiveClassifier()
+            classifier.fit(X_train, y_train, size)
+            acc, loss = classifier.predict(X_test, y_test)
+            loss_per_size.append(loss)
+        loss_per_split.append(loss_per_size)
+    avg = [(sum(col)) / len(col) for col in zip(*loss_per_split)]
+    if verbose:
+        graphPlotAndShow(test_size, avg, "test size", "loss")
+        zipped = list(zip(test_size, avg))
+        zipped.sort(key=lambda x: x[1])
+        best_size = zipped[0]
+        print(f"Kfold cross validation results:\n"
+              f"Best size is={best_size[0]} with loss={best_size[1]}")
 
 
 if __name__ == "__main__":
@@ -88,6 +165,9 @@ if __name__ == "__main__":
     classifier.fit(train_x, train_y)
     # predicting on the test data set
     accuracy, loss = classifier.predict(test_x, test_y)
-    print(accuracy)
     if args.verbose:
         print(f"loss with cost optimizing={loss}")
+    else:
+        print(loss)
+
+    # experiment(verbose=args.verbose)
